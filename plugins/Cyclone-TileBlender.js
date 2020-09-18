8f11f895aad12ad2dac9f8586d51981e15f47546
Brian Hudell <brian@hudell.com>
2020-09-18 02:53:16 -0300

    new test data

--- plugins/Cyclone-TileBlender.js
+++ plugins/Cyclone-TileBlender.js
@@ -505,11 +505,11 @@ CycloneTileBlender.patchClass(Spriteset_Map, $super => class {
     CycloneTileBlender.clearBitmapCache();
     this._blenderTileSprites = [];
 
-    this._blenderTileSprites.push(new SpriteBlenderTile([2912, 2856, 184], 2, 3));
-    this._blenderTileSprites.push(new SpriteBlenderTile([2912, 2854, 185], 3, 3));
+    this._blenderTileSprites.push(new SpriteBlenderTile([2864, 2832, 184], 2, 3));
+    this._blenderTileSprites.push(new SpriteBlenderTile([2864, 2816, 185], 3, 3));
 
-    this._blenderTileSprites.push(new SpriteBlenderTile([2912, 2856], 5, 3));
-    this._blenderTileSprites.push(new SpriteBlenderTile([2912, 2854], 6, 3));
+    this._blenderTileSprites.push(new SpriteBlenderTile([2864, 2849], 5, 3));
+    this._blenderTileSprites.push(new SpriteBlenderTile([2864, 2861], 6, 3));
 
     // for (const {tileId, x, y, tag } of $gameMap.priorityTiles()) {
     //   this._blenderTileSprites.push(new SpriteBlenderTile(tileId, x, y, tag));
