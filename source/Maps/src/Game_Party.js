dabc0f6ccb3966951bddc2a7584c5d529732fdc7
Brian Hudell <brian@hudell.com>
2022-07-17 19:53:27 -0300

    Replaced old maps source

--- source/Maps/src/Game_Party.js
+++
@@ -1,9 +0,0 @@
-CycloneMaps.patchClass(Game_Party, $super => class {
-  onPlayerWalk() {
-    $super.onPlayerWalk.call(this);
-
-    if (CycloneMaps.commonEventRegions.size > 0) {
-      CycloneMaps.checkRegionActions();
-    }
-  }
-});
